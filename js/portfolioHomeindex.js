199269aa0285167c135afa8bfb0ce79e92548d98
Jillbills <87965054+Jillbills@users.noreply.github.com>
2022-01-13 15:26:59 -0800

    Making media queries more responsive

--- js/portfolioHomeindex.js
+++ js/portfolioHomeindex.js
@@ -1,5 +1,5 @@
 
-// Sticky header feature - shows hero first, then scroll to stick header.
+// STICKY HEADER feature - shows hero first, then scroll to stick header.
 window.onscroll = function() {myFunction()};
     
 var header = document.getElementById("myHeader");
@@ -15,7 +15,40 @@ function myFunction() {
 
 
 
-// Modal when  you click Submit button in the Connect section of my homepage 
+
+// Not currently active - need to reactivate HTML for this to work 
+// MODAL when you click Explore button in the About section of my homepage 
+// Get the modal
+// var modal = document.getElementById("modalSOL");
+
+// // Get the button that opens the modal
+// var btn = document.getElementById("SOLbutton");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// //   modal.style.display = "#modalSOL"; 
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+
+
+// Not currently active - need to reactivate HTML for this to work 
+// MODAL when  you click Submit button in the Connect section of my homepage 
 // Get the modal
 var modal = document.getElementById("modalConnect");
 
@@ -27,7 +60,8 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal.style.display = "block";
+//   modal.style.display = "#modalConnect";  ???
+//   modal.style.display = "block"; this was the original code but not working - probably because I changed the names?
 }
 
 // When the user clicks on <span> (x), close the modal
