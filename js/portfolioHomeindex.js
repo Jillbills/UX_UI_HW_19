7913914ff2bf5b3562c41170ace47ff4ceeeb9af
Jillbills <87965054+Jillbills@users.noreply.github.com>
2022-01-13 11:14:11 -0800

    Coding modals

--- js/portfolioHomeindex.js
+++ js/portfolioHomeindex.js
@@ -14,3 +14,31 @@ function myFunction() {
 }
 
 
+
+// Modal when  you click Submit button in the Connect section of my homepage 
+// Get the modal
+var modal = document.getElementById("modalConnect");
+
+// Get the button that opens the modal
+var btn = document.getElementById("connectButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
